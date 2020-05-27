6fae7617d72fd9c17792b03c528b16a37364e496
tampler <socnetfpga@gmail.com>
2020-05-27 18:15:23 +0300

    Finished initial impl

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,6 @@
 package demo
 
-import zio.{ App, Promise, Ref, Task, ZIO }
+import zio.{ App, Ref, Task, ZIO }
 import zio.blocking.{ blocking, Blocking }
 import zio.console.{ putStrLn, Console }
 import Common._
@@ -20,31 +20,28 @@ object Common {
 
     override def registerListener(listener: Splash[Particle]) = ()
 
-    val env = Console.live ++ Blocking.live
+    private val env = Console.live ++ Blocking.live
 
-    def listener(n: Int, input: Particle) = println(s"[${Thread.currentThread().getName}] Listener$n: caught $input")
+    private def listener(n: Int, input: Particle) =
+      println(s"[${Thread.currentThread().getName}] Listener$n: caught $input")
 
-    val list0 = (p: Particle) => listener(0, p)
-    val list1 = (p: Particle) => listener(1, p)
+    private val list0 = (p: Particle) => listener(0, p)
+    private val list1 = (p: Particle) => listener(1, p)
 
-    def emit(num: Int): ZIO[zio.blocking.Blocking with zio.console.Console, Nothing, Particle] =
+    private def emit(num: Int): ZIO[zio.blocking.Blocking with zio.console.Console, Nothing, Particle] =
       for {
         _    <- blocking(ZIO.effectTotal(Thread.sleep(1000)))
         part = Particle(Random.between(0, 1000), Random.between(-1, 2))
         _    <- putStrLn(s"[${Thread.currentThread().getName}] Emitted $part")
       } yield part
 
-    // def regEff = ZIO.effect(registerListener(???))
-
-    def startEff(n: Int) =
+    private def startEff(n: Int) =
       for {
-        // p         <- Promise.make[Nothing, Unit]
         listeners <- Ref.make(Vector.empty[Splash[Particle]])
         _         <- listeners.update(_ :+ list0)
         _         <- listeners.update(_ :+ list1)
-        particles <- ZIO.foreach(Range(0, n))(emit)
-        _         <- listeners.get.map(_.foreach(v => v(particles.head)))
-        // _ <- p.await
+        particles <- ZIO.foreachParN(n)(Range(0, n))(emit)
+        _         <- listeners.get.map(_.foreach(v => particles.foreach(p => v(p))))
       } yield ()
 
     override def start(n: Int) = startEff(n).provideLayer(env)
@@ -58,5 +55,5 @@ object Main extends App {
 
   val gaiger = new GeigerZIOEff()
 
-  val prog = gaiger.start(2)
+  val prog = gaiger.start(3)
 }
